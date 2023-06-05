2667d9806fd938137feed08f4ec6af7a15e1f476
Dexter Punzalan <dexterpunzalan23@gmail.com>
2023-06-05 14:57:26 +0800

    chore: responsive menu header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
+import { useReducer } from 'react'
+import { Menu } from '@mui/icons-material'
 import myLogo from '/icons/dp-icon.svg'
 import styles from './index.module.scss'
 
 const Header = () => {
+  const [toggle, toggleMenu] = useReducer((value) => !value, false )
+
   return (
     <div className={styles.header}>
       <div className={styles.header__logo}>
         <img src={myLogo} alt="My logo" />
       </div>
-      <div className={styles.header__links}>
+      <div className={styles.header__menu} onClick={toggleMenu}>
+        <Menu />
+      </div>
+      <div className={[styles.header__links, toggle ? styles['header__links--is-show'] : ''].join(' ')}>
         <div>
           <a href="/#"> Me </a>
         </div>
